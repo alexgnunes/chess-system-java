db514de5d34fce83d5c2e886a6d233ab351ef538
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-08-13 17:40:19 -0300

    Start to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,10 +6,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 5);
+		Board board = new Board(8,8;);
 		
-		System.out.println(pos);
-
 	}
 
 }
