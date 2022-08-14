db57bced3e424c59416cdc11f262d9bd6fd00db9
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-08-14 19:08:53 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targPosition.toPosition();
 		ValidateSourcePosition(source);
+		ValidateTargetPosition(source,target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
@@ -49,6 +50,12 @@ public class ChessMatch {
 		}
 	}
 
+	private void ValidateTargetPosition (Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("the chosen piece can´t move to target position");
+		}
+	}
+	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
