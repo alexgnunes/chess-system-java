39f131af738792dad732dc3715870112287e40ef
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-08-18 06:03:43 -0300

    Bishop

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -69,7 +69,7 @@ public class King extends ChessPiece {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		// sw
+		// se
 		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
